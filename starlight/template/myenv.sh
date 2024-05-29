3d378ddebf76ec45088a006ea96a91400ce83771
kristof de spiegeleer <kristof@incubaid.com>
2024-05-29 07:22:44 +0300

    doc

--- starlight/template/myenv.sh
+++ starlight/template/myenv.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-set -x
 
 # Define base directory
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export BASE_DIR
-export NODE_DIR="$BASE_DIR/node_modules"
 
-if ! command -v nvm &> /dev/null; then
-    echo "run install script"
-    exit 1
-fi
+# if ! command -v nvm &> /dev/null; then
+#     echo "error, can't find nvm, run the install script"
+#     exit 1
+# fi
 
 if ! command -v npm &> /dev/null; then
-    echo "run install script"
+    echo "error, can't find npm, run the install script"
     exit 1
 fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 nvm use 20.14.0
 
 # Move to base directory and check if node modules directory exists
 cd $BASE_DIR
+
+export NODE_DIR="$BASE_DIR/node_modules"
+
 
 if [ ! -d "$NODE_DIR" ]; then
     echo "Directory $NODE_DIR does not exist"
--- starlight/template/start.sh
+++ starlight/template/start.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
-# Define base directory
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export BASE_DIR
-
 cd $BASE_DIR
+source myenv.sh
+
+#following opens the webpage on osx and waits 3 sec before going there
+if [ "$(uname)" == "Darwin" ]; then
+    session_name="linkopen"    
+    if screen -list | grep -q "\.${session_name}"; then
+        screen -S $session_name -X quit
+    fi
+    screen -dmS $session_name bash -c "sleep 3 && open http://localhost:4321/"
+fi
 
 npm run dev
